3d80a0018969e6730592925fe5533e6747290c2f
bambang yudhotomo <bambangyudhotomo@gmail.com>
2014-10-25 01:03:11 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require "bin/konfigurasi.php"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Bambang Yudhotomo" />
+<title>Sample map polyline</title>
+
+<script type="text/javascript" src="<?php echo $rootdir.$jsdir; ?>jquery-2.1.0.min.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $rootdir.$cssdir; ?>style.css" />
+<script type="text/javascript" src="<?php echo $rootdir.$jsdir; ?>map-proc.js"></script>
+</head>
+
+<body>
+<div id="map-canvas"></div>
+</body>
+</html>
