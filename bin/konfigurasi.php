f9464e1488500203c6695274127d23fa7a322c38
bambang yudhotomo <bambangyudhotomo@gmail.com>
2014-10-25 00:41:55 +0700

    Create konfigurasi.php

---
+++ bin/konfigurasi.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This files contain setting parameters
+ * By Bambang Yuhdotomo
+ * Kredit :
+ * www.google.com
+*/
+
+
+$host				= "localhost";
+$database			= "test_map";
+$username			= "root";
+$password			= "";
+$rootdir			= "http://localhost/sample-map/";
+$jsdir				= "js/";
+$cssdir				= "css/";
+
+?>
